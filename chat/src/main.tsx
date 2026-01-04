0b9784eba1e9902d11f07513839819a612c382b6
hzrd149 <github@hzrd149.com>
2026-01-04 14:29:10 -0600

    setup vite app with shadcn and react router

---
+++ chat/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./components/theme-providers";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>,
+);
